e660c124edba4a2e4c73b4d09339696ceb92751b
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-11 11:13:01 -0700

    getting shift to pass test

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { push, pop, unshift } = require("./index.js");
+const { push, pop, unshift, shift } = require("./index.js");
 
 describe("push", () => {
   it("should add an element to an array", () => {
@@ -7,16 +7,16 @@ describe("push", () => {
     expect(arr.length).toBe(4);
     expect(arr[3]).toBe("d");
   });
-})
+});
 
 describe("pop", () => {
   it("should remove an element from an array", () => {
     const arr = ["a", "b", "c"];
-    expect(pop(arr)).toBe('c');
+    expect(pop(arr)).toBe("c");
     expect(arr.length).toBe(2);
     expect(arr[1]).toBe("b");
   });
-})
+});
 
 describe("unshift", () => {
   it("should add an element to the beginning of an array", () => {
@@ -24,5 +24,14 @@ describe("unshift", () => {
     expect(unshift(arr, "test")).toBe(4);
     expect(arr.length).toBe(4);
     expect(arr[0]).toBe("test");
+  });
+});
+
+describe("shift", () => {
+  it("should remove an element from the beginning of an array", () => {
+    const arr = ["a", "b", "c"];
+    expect(shift(arr)).toBe(2);
+    expect(arr.length).toBe(2);
+    expect(arr[0]).toBe("b");
   });
 });
